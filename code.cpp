28aa6361acff31c0473277ba0dfe3a67106dabae
Agustín <agustinespindola@gmail.com>
2015-04-16 04:51:14 -0300

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,15 @@
+/* TP1: Tipos de datos, Agustín Espíndola K1091, 16/04/15 */
+
+#include <iostream>
+
+int elLoco = 22;
+char* miNombre = "Agustin";
+int aniosCarrera = 5;
+int graduado = elLoco + aniosCarrera;
+bool soyIngeniero = false;
+
+int main () {
+std::cout << "Nombre: " << miNombre << "\n \n"; 
+std::cout << "Edad: " << elLoco << "\n \n";
+std::cout << "Me recibo a los " << graduado;
+}
